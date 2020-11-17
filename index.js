2a79550f425b34f408ddc4e9b2247df424474565
Anil <anil.shafeeque@gmail.com>
2020-11-17 20:36:16 +0300

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 app.get('/', (req, res) => {
 //  res.send('<h1>Hello world</h1>');
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/audiorec.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected from browser');
+//io.on('connection', (socket) => {
+ // console.log('a user connected from browser');
+  io.on('connect', (client) => {
+    console.log(`Client connected [id=${client.id}]`);
+    client.emit('server_setup', `Server connected [id=${client.id}]`);
 
-  socket.on('chat message', (msg) => {
+  client.on('chat message', (msg) => {
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
-  socket.on('disconnect', () => {
+
+  client.on('message-transcribe', async function(data) {
+    // we get the dataURL which was sent from the client
+    const dataURL = data.audio.dataURL.split(',').pop();
+    // we will convert it to a Buffer
+    let fileBuffer = Buffer.from(dataURL, 'base64');
+    // run the simple transcribeAudio() function
+    console.log('Calling Speach API ');
+    
+
+    
+    fs.writeFileSync('test.wav', fileBuffer);
+
+    //const results = await transcribeAudio(fileBuffer);
+    //client.emit('results', results);
+});
+
+  client.on('disconnect', () => {
     console.log('user disconnected from the bowser');
   });
 });
