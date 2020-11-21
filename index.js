cbce7b7fa12b3821d3d2ce96f46f5aecc34be796
Anil <anil.shafeeque@gmail.com>
2020-11-21 09:10:50 +0300

    added key vault integration

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var azure = require('azure-storage');
+const {SecretClient} = require('@azure/keyvault-secrets');
+const {DefaultAzureCredential, ManagedIdentityCredential} = require('@azure/identity');
+const { ClientSecretCredential } = require("@azure/identity");
+
+var tenantId = "a4a1ba9b-8ece-4448-a5dc-d891fad077a3";
+var clientId = "e652b0c8-603e-4314-a46d-4f938b69f471";
+var clientSecret = "c-AYPWPG637.bHxT526vK~Lpyf6Y3~5zQF";
+
+//const credential = new ManagedIdentityCredential();
+//const credential = new DefaultAzureCredential();
+const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+// Replace value with your Key Vault name here
+const vaultName = "sttkeys-kv";
+const url = `https://${vaultName}.vault.azure.net`;
+  
+const client = new SecretClient(url, credential);
+
+// Replace value with your secret name here
+const secretName = "storagekey";
+
+
 
 app.get('/', (req, res) => {
 //  res.send('<h1>Hello world</h1>');
@@ -29,12 +51,22 @@ app.get('/', (req, res) => {
     console.log('Calling Speach API ');
     
 
+    // Create the wave file in the local storage
+      fs.writeFileSync('test.wav', fileBuffer);
+
     
-    fs.writeFileSync('test.wav', fileBuffer);
+    
+    // Calling the function to retreive the storage connect string stored in azure keyvault secret
+    const stsec = await fetchsecret();
+ 
+
+
+
+
 
   // Upload the audio file to azure storage account  
   // Enter the connect string  nnnnn
-    var blobService = azure.createBlobService("DefaultEndpointsProtocol=https;AccountName=storagexxxxxxxxxxxxxxxxx");
+    var blobService = azure.createBlobService(stsec);
     blobService.createBlockBlobFromLocalFile('images', 'testinazure.wav', 'test.wav', function(error, result, response) {
     if (!error) {
     console.log('File uploaded ');
@@ -53,3 +85,11 @@ app.get('/', (req, res) => {
 http.listen(5000, () => {
   console.log('listening on *:5000');
 });
+
+// Function to retreive the storage connect string from the azure keyvault
+async function fetchsecret() {
+  console.log('Going for Secret');
+  const retrievedSecret = await client.getSecret(secretName);
+  //console.log(`Your secret value is: ${retrievedSecret.value}`);
+  return retrievedSecret.value;
+}
